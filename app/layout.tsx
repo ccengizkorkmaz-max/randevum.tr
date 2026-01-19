ccf618de943ac62d529f848e5ab5992aa04338e0
Cengiz Korkmaz <cengiz.korkmaz@negmargroup.com>
2026-01-19 15:56:10 +0300

    feat: add google tag manager scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
@@ -20,6 +21,19 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased min-h-screen bg-zinc-50`}>
         {children}
         <Toaster />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LPSMG5XLQT"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-LPSMG5XLQT');
+          `}
+        </Script>
       </body>
     </html>
   );
